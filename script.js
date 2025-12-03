1d51fbd750eebbbc067389a68c51945c063a2e84
Jonathan Nassif <jonas2000288@gmail.com>
2025-12-03 14:24:35 +0200

    Compressed images

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".mobile-menu-btn").addEventListener("click", () => {
-  document.querySelector(".mobile-nav").style.display = "flex";
-});
-document.querySelectorAll(".mobile-nav a").forEach((link) => {
-  link.addEventListener(
-    "click",
-    () => (document.querySelector(".mobile-nav").style.display = "none")
-  );
-});
